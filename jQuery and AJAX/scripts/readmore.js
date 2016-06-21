d648002f4fef659507bcbcd5e8b015a541eb7b63
baxlash <yaroslav.dimitrov@outlook.com>
2016-06-21 19:24:40 +0300

    mid version

---
+++ jQuery and AJAX/scripts/readmore.js
@@ -0,0 +1,5 @@
+function readMore() {
+    let moreText = " Hello, User!";
+    document.getElementById("link").style.visibility="hidden";
+    document.getElementById("text").innerHTML += moreText;
+}
